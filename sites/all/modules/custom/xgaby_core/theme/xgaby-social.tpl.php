4780e05d5ed76c8f9c92a07d0f5521ccea7cb6cf
Cornel Andreev <hugronaphor@gmail.com>
2015-05-24 13:04:51 +0100

    Open social links in new tab.

--- sites/all/modules/custom/xgaby_core/theme/xgaby-social.tpl.php
+++ sites/all/modules/custom/xgaby_core/theme/xgaby-social.tpl.php
@@ -3,19 +3,19 @@
     <div class="social-buttons">
       <ul class="footer-social">
         <?php if ($twitter_site_url) : ?>
-        <li rel="twitter"><a title="Twitter" href="<?php print $twitter_site_url; ?>"></a></li>      
+        <li rel="twitter"><a title="Twitter" href="<?php print $twitter_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
         <?php if ($youtube_site_url) : ?>
-          <li rel="yt"><a title="YouTube" href="<?php print $youtube_site_url; ?>"></a></li>
+          <li rel="yt"><a title="YouTube" href="<?php print $youtube_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
         <?php if ($instagram_site_url) : ?>
-          <li rel="instagram"><a title="Instagram" href="<?php print $instagram_site_url; ?>"></a></li>
+          <li rel="instagram"><a title="Instagram" href="<?php print $instagram_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
         <?php if ($skype_site_url) : ?>
           <li rel="skype"><a title="Skype" href="<?php print $skype_site_url; ?>"></a></li>
         <?php endif; ?>
         <?php if ($gplus_site_url) : ?>
-          <li rel="gplus"><a title="Google+" href="<?php print $gplus_site_url; ?>"></a></li>
+          <li rel="gplus"><a title="Google+" href="<?php print $gplus_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
       </ul>
     </div>
