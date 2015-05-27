11940243c9dd801d79cbe759de074a5765beb695
Cornel Andreev <hugronaphor@gmail.com>
2015-05-27 22:12:01 +0100

    Re-make the blog page.

--- sites/all/themes/xgaby/js/main.js
+++ sites/all/themes/xgaby/js/main.js
@@ -2,22 +2,48 @@
 
   'use strict';
 
-  /**
-   * A simple Drupal behavior example.
-   */
   Drupal.behaviors.homepageSlider = {
     attach: function(context) {
 
       $('#homepage-pager').on("click", "a", function() {
         // Change the background, I guess.
         // .................
-        
-        
+
+
         $('#homepage-pager a').removeClass('selected');
         $(this).addClass('selected');
         return false;
       });
 
+    }
+  };
+
+  Drupal.behaviors.blogIndexBehavior = {
+    attach: function(context) {
+
+      $(document).on("click", ".page-blog .read-more-btn span", function() {
+        var parentWrapper = $(this).parents('article'),
+          nid = $(this).parent().attr('data-nid'),
+          url = '/node/get/ajax/' + nid + '/full';
+
+        $.get(url, function(data) {
+          parentWrapper.html(data);
+        });
+
+        return false;
+      });
+
+      $(document).on("click", ".page-blog .read-less-btn span", function() {
+        var parentWrapper = $(this).parents('article'),
+          nid = $(this).parent().attr('data-nid'),
+          url = '/node/get/ajax/' + nid + '/teaser';
+
+        $.get(url, function(data) {
+          parentWrapper.html(data);
+        });
+
+        return false;
+      });
     }
   };
 
