58fd90d19b8907eb63eeeb3a345dfe6f6df726c7
Cornel Andreev <hugronaphor@gmail.com>
2015-06-08 21:22:19 +0100

    Add admin_views and refresh some styles.

--- sites/all/modules/custom/xgaby_core/theme/xgaby-social.tpl.php
+++ sites/all/modules/custom/xgaby_core/theme/xgaby-social.tpl.php
@@ -3,19 +3,13 @@
     <div class="social-buttons">
       <ul class="footer-social">
         <?php if ($twitter_site_url) : ?>
-        <li rel="twitter"><a title="Twitter" href="<?php print $twitter_site_url; ?>" target="_blank"></a></li>
+          <li rel="twitter"><a title="Twitter" href="<?php print $twitter_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
-        <?php if ($youtube_site_url) : ?>
-          <li rel="yt"><a title="YouTube" href="<?php print $youtube_site_url; ?>" target="_blank"></a></li>
+        <?php if ($vine_site_url) : ?>
+          <li rel="vine"><a title="Vine" href="<?php print $vine_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
-        <?php if ($instagram_site_url) : ?>
-          <li rel="instagram"><a title="Instagram" href="<?php print $instagram_site_url; ?>" target="_blank"></a></li>
-        <?php endif; ?>
-        <?php if ($skype_site_url) : ?>
-          <li rel="skype"><a title="Skype" href="<?php print $skype_site_url; ?>"></a></li>
-        <?php endif; ?>
-        <?php if ($gplus_site_url) : ?>
-          <li rel="gplus"><a title="Google+" href="<?php print $gplus_site_url; ?>" target="_blank"></a></li>
+        <?php if ($vimeo_site_url) : ?>
+          <li rel="vimeo"><a title="Vimeo" href="<?php print $vimeo_site_url; ?>" target="_blank"></a></li>
         <?php endif; ?>
       </ul>
     </div>
